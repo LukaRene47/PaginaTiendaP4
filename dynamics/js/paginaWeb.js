f18a80d45b9bb376b8f160595bf5ba7f76ed91b6
LukaRene47 <324008915@alumno.enp.unam.mx>
2025-06-16 12:52:57 -0600

    primera version pagina

---
+++ dynamics/js/paginaWeb.js
@@ -0,0 +1,20 @@
+
+var seccionProductos = document.getElementById("productos");
+productos.appendChild();    
+const productos = [{id:1, nombre:"casco", precio:690, imagen:"../statics/img/casco.png" },
+    {id:2, nombre: "Guantes", precio: 400, imagen:"" },
+    {id:3, nombre:"Luces para bicicleta", precio: 789, imagen:"" },
+    {id:4, nombre:"Asiento para bicicleta", precio:1200, imagen:""},
+    {id:5, nombre:"Bombas de aire", precio: 650, imagen: ""},
+    {id:6, nombre:"Portabicicletas", precio: 1700, imagen:""},
+    {id:7, nombre:"Tenis", precio: 599, imagen:""},
+    {id:8, nombre:"Bicicletas de montaña", precio: 4600, imagen:""},
+    {id:9, nombre:"Playera deportivas", precio:900, imagen:""},
+]   
+// no nos va a dar tiempo F
+let carritoLista = document.getElementbyId("ticket");
+/*for(let i=0; i<productos.length; i++)
+{
+    document.createElement("div");
+}
+    */
